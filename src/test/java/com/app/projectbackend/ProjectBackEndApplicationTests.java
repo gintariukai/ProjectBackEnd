075bbd369c5b15028bb589e31ac43071a72ea2c3
gintariukai <gintariukai@hotmail.com>
2023-03-20 11:20:05 +0200

    Initial commit

---
+++ src/test/java/com/app/projectbackend/ProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.projectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
