59bfec79d157b7c42f74f7b2de37e32d9566a92e
gintariukai <gintariukai@hotmail.com>
2023-03-20 16:12:39 +0200

    Added shop section development

--- src/main/java/com/app/projectbackend/controllers/MainController.java
+++ src/main/java/com/app/projectbackend/controllers/MainController.java
@@ -5,12 +5,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ProductController {
+public class MainController {
 
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "Home page");
         return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About page");
+        return "about";
     }
 
 }
---
+++ src/main/java/com/app/projectbackend/controllers/ShopController.java
@@ -0,0 +1,14 @@
+package com.app.projectbackend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShopController {
+
+    @GetMapping("/shop")
+    public String shopMain(Model model) {
+        return "shop-main";
+    }
+}
