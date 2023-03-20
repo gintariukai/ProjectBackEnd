1c0efb007a6e56c5371899dc877599d9f766fbca
gintariukai <gintariukai@hotmail.com>
2023-03-20 21:58:20 +0200

    Added editing and deleting products

--- src/main/java/com/app/projectbackend/controllers/ProductController.java
+++ src/main/java/com/app/projectbackend/controllers/ProductController.java
@@ -50,4 +50,33 @@ public class ProductController {
         model.addAttribute("product", res);
         return "product-details";
     }
+
+    @GetMapping("/product/{id}/edit")
+    public String productEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!productRepository.existsById(id)) {
+            return "redirect:/shop";
+        }
+        Optional<Product> product = productRepository.findById(id);
+        ArrayList<Product> res = new ArrayList<>();
+        product.ifPresent(res::add);
+        model.addAttribute("product", res);
+        return "product-edit";
+    }
+
+    @PostMapping("product/{id}/edit")
+    public String shopProductUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam BigDecimal price, @RequestParam String description, Model model) {
+        Product product = productRepository.findById(id).orElseThrow();
+        product.setTitle(title);
+        product.setPrice(price);
+        product.setDescription(description);
+        productRepository.save(product);
+        return "redirect:/shop";
+    }
+
+    @PostMapping("product/{id}/remove")
+    public String shopProductDelete(@PathVariable(value = "id") long id, Model model) {
+        Product product = productRepository.findById(id).orElseThrow();
+        productRepository.delete(product);
+        return "redirect:/shop";
+    }
 }
